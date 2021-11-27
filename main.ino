5382215760be329570182a3e7228606b72430118
Taim <taldabba@gmail.com>
2021-11-27 02:32:33 -0500

    LED fun

--- main.ino
+++ main.ino
@@ -1,12 +1,34 @@
-int powerPin = 11;
-int delayTime = 750;
+int ledWire1 = 12;
+int ledWire2 = 11;
+int ledWire3 = 10;
+int ledWire4 = 9;
+int ledWire5 = 8;
+int ledWire6 = 7;
+int ledArray[6] = {ledWire1, ledWire2, ledWire3, ledWire4, ledWire5, ledWire6};
+int delayTime = 50;
+int led = 0;
 
 void setup() {
-    pinMode(powerPin, OUTPUT);
+    for (int i=0; i<5; i++) {
+        pinMode(ledArray[i], OUTPUT);
+    }
 }
 void loop() {
-    digitalWrite(powerPin, HIGH);
+    for (int i=0; i<5; i++) {
+        digitalWrite(ledArray[i], HIGH);
+        delay(delayTime);
+        digitalWrite(ledArray[i], LOW);
+        led=i;
+    }
+    led++;
+    digitalWrite(ledArray[led], HIGH);
     delay(delayTime);
-    digitalWrite(powerPin, LOW);
-    delay(delayTime); 
+    digitalWrite(ledArray[led], LOW);
+    led--;
+    while (led>0) {
+        digitalWrite(ledArray[led], HIGH);
+        delay(delayTime);
+        digitalWrite(ledArray[led], LOW);
+        led--;
+    }
 } 
